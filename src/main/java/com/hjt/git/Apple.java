57aa7bfd493387a71cfe2041f970e285782365d6
Hhjjttao <Hhjjttao@163.com>
2018-10-08 09:46:44 +0800

    modify 3

--- src/main/java/com/hjt/git/Apple.java
+++ src/main/java/com/hjt/git/Apple.java
@@ -3,5 +3,9 @@ package com.hjt.git;
 public class Apple {
 	public static void main(String[] args) {
 		System.out.println("left!!!!");
+		System.out.println("middle....");
+		System.out.println("1111");
+		System.out.println("2222");
+		System.out.println("right!!!!");
 	}	
 }
